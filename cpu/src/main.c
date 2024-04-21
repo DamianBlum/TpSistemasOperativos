8780e89a9b7f5fc9726d9376fb530cb559fdf3bb
Elian Benitez <febenitez@frba.utn.edu.ar>
2024-04-21 23:27:31 +0000

    Solucione los problemas con la funcion SET

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "cicloDeInstruccion.h"
 
 // Servidor
 int socket_servidor_dispatch;
@@ -48,21 +47,23 @@ int main(int argc, char *argv[])
     */
     // PARTE SERVIDOR
 
+    /*
     pthread_create(&tid[DISPATCH], NULL, servidor_dispatch, NULL);
 
     pthread_create(&tid[INTERRUPT], NULL, servidor_interrupt, NULL);
-
+    */
     // acto de ejectar una instruccion
 
-    fetch(linea_de_instruccion, registros->PC, logger);
+    fetch();
     log_debug(logger, "LOG DESPUES DEL FETCH: %s", linea_de_instruccion);
 
-    decode(linea_de_instruccion, &instruccion, linea_de_instruccion_separada, logger);
+    decode();
     log_debug(logger, "LOG DESPUES DEL execute: %s", linea_de_instruccion_separada[0]);
     log_debug(logger, "LOG DESPUES DEL execute: %s", linea_de_instruccion_separada[1]);
     log_debug(logger, "LOG DESPUES DEL execute: %s", linea_de_instruccion_separada[2]);
 
-    execute(linea_de_instruccion_separada, instruccion, registros, logger);
+    execute();
+    log_debug(logger, "VALOR DEL REGISTRO AX: %d ", registros->AX );
 
     /*
     while (1) 
@@ -93,9 +94,11 @@ int main(int argc, char *argv[])
         registros->PC++;
     }
     */
+
+    /*
     pthread_join(tid[DISPATCH], NULL);
     pthread_join(tid[INTERRUPT], NULL);
-
+    */
 
 
     // liberar_conexion(cliente_memoria, logger); cuando pruebe lo de ser cliente de memoria descomentar esto
@@ -106,7 +109,6 @@ int main(int argc, char *argv[])
     destruir_config(config);
     destruir_logger(logger);
     string_array_destroy(linea_de_instruccion_separada);
-
     return 0;
 }
 
@@ -183,6 +185,200 @@ void *servidor_interrupt(void *arg) // por aca va a recibir un bit cuando quiere
             log_error(logger, "Recibi una operacion rara (%d), termino el servidor.", operacion);
             return EXIT_FAILURE;
         }
+    }
+    return EXIT_SUCCESS;
+}
+
+e_instruccion parsear_instruccion(char* instruccionString){
+    e_instruccion enum_instruccion;
+
+    if (string_equals_ignore_case(instruccionString,"SET")) {
+        enum_instruccion = SET;
+    }
+    else if (string_equals_ignore_case(instruccionString,"MOV_IN")){
+        enum_instruccion = MOV_IN;
+    }
+    else if (string_equals_ignore_case(instruccionString,"MOV_OUT")){
+        enum_instruccion = MOV_OUT;
+    }
+    else if (string_equals_ignore_case(instruccionString,"SUM")){
+        enum_instruccion = SUM;
+    }
+    else if (string_equals_ignore_case(instruccionString,"SUB")) {
+        enum_instruccion = SUB;
+    }
+    else if (string_equals_ignore_case(instruccionString,"JNZ")){
+        enum_instruccion = JNZ;
+    }
+    else if (string_equals_ignore_case(instruccionString,"RESIZE")){
+        enum_instruccion = RESIZE;
+    }
+    else if (string_equals_ignore_case(instruccionString,"COPY_STRING")){
+        enum_instruccion = COPY_STRING;
+    }
+    else if (string_equals_ignore_case(instruccionString,"WAIT")){
+        enum_instruccion = WAIT;
+    }
+    else if (string_equals_ignore_case(instruccionString,"SIGNAL")){
+        enum_instruccion = SIGNAL;
+    }
+    else if (string_equals_ignore_case(instruccionString,"IO_GEN_SLEEP")){
+        enum_instruccion = IO_GEN_SLEEP;
+    }
+    else if (string_equals_ignore_case(instruccionString,"IO_STDIN_READ")){
+        enum_instruccion = IO_STDIN_READ;
+    }
+    else if (string_equals_ignore_case(instruccionString,"IO_STDOUT_WRITE")){
+        enum_instruccion = IO_STDOUT_WRITE;
+    }
+    else if (string_equals_ignore_case(instruccionString,"IO_FS_CREATE")){
+        enum_instruccion = IO_FS_CREATE;
+    }
+    else if (string_equals_ignore_case(instruccionString,"IO_FS_DELETE")){
+        enum_instruccion = IO_FS_DELETE;
+    }
+    else if (string_equals_ignore_case(instruccionString,"IO_FS_TRUNCATE")){
+        enum_instruccion = IO_FS_TRUNCATE;
+    }
+    else if (string_equals_ignore_case(instruccionString,"IO_FS_WRITE")){
+        enum_instruccion = IO_FS_WRITE;
+    }
+    else if (string_equals_ignore_case(instruccionString,"IO_FS_READ")){
+        enum_instruccion = IO_FS_READ;
+    }
+    else if (string_equals_ignore_case(instruccionString,"EXIT")) {
+        enum_instruccion = INSTRUCTION_EXIT;    
+    }
+    return enum_instruccion;
+}
+
+
+void fetch() 
+{
+    //Buscamos la siguiente instruccion con el pc en memoria y la asignamos a la variable instruccion
+    // char instr_recibida = recibir_mensaje(socket, logger); para conseguir la instruccion
+    // por ahora lo hacemos default
+    linea_de_instruccion= string_duplicate("SET AX 10");
+    log_debug(logger, "LA instruccion leida es %s", linea_de_instruccion);
+    return EXIT_SUCCESS;
+}
+
+void decode(){
+
+    log_debug(logger, linea_de_instruccion);
+    linea_de_instruccion_separada = string_split(linea_de_instruccion, " " );
+
+    log_debug(logger, linea_de_instruccion_separada[0]);
+    log_debug(logger, linea_de_instruccion_separada[1]);
+    log_debug(logger, linea_de_instruccion_separada[2]);
+
+    instruccion = parsear_instruccion(linea_de_instruccion_separada[0]);
+    log_debug(logger, "Instruccion: %s", linea_de_instruccion_separada[0]);
+    log_debug(logger, "Registro: %s", linea_de_instruccion_separada[1]);
+    log_debug(logger, "Valor: %s", linea_de_instruccion_separada[2]);
+
+    return EXIT_SUCCESS;
+    // ["SET","BX","10"]
+    //chararg1 = instr_spliteado[1]; para acceder a algun elemento
+}
+
+void execute() {
+    log_debug(logger, "ENTRE AL EXECUTE");
+    switch (instruccion)
+    {
+    case SET:
+        log_debug(logger, "ESTOY EN EL CASO SET");
+        instruction_set();
+        break;
+    case MOV_IN:
+        break;
+    case MOV_OUT:
+        break;
+    case SUM:
+        break;
+    case SUB:
+        break;
+    case JNZ:
+        break;
+    case RESIZE:
+        break;
+    case COPY_STRING:
+        break;
+    case WAIT:
+        break;
+    case SIGNAL:
+        break;
+    case IO_GEN_SLEEP:
+        break;
+    case IO_STDIN_READ:
+        break;
+    case IO_STDOUT_WRITE:
+        break;
+    case IO_FS_CREATE:
+        break;
+    case IO_FS_DELETE:
+        break;
+    case IO_FS_TRUNCATE:
+        break;
+    case IO_FS_WRITE:
+        break;
+    case IO_FS_READ:
+        break;
+    case INSTRUCTION_EXIT:
+        break;
+    default:
+        log_error(logger, "instruccion incorreta");
+        break;
+    }
+}
+
+void instruction_set(){
+    log_debug(logger, "ESTOY EN INSTRUCTION SET");
+    char* registroDestino = string_new();
+    char* valorEnString = string_new();
+
+    registroDestino = linea_de_instruccion_separada[1];
+    log_debug(logger, "Registro Destino: %s", registroDestino);
+
+    valorEnString = linea_de_instruccion_separada[2];
+    log_debug(logger, "Valor en string: %s", valorEnString);
+
+    int valorEnInt = atoi(valorEnString);
+    log_debug(logger, "Valor en string: %d", valorEnInt);
+
+    // Establecer el valor en el registro correspondiente
+    asignarValoresIntEnRegistros(registroDestino, valorEnInt, "SET");
+
+    free(registroDestino);
+    free(valorEnString);
+    return EXIT_SUCCESS;
+}
+
+void asignarValoresIntEnRegistros(char* registroDestino, int valor, char* instruccion) {
+
+    if (strcmp(registroDestino, "AX") == 0) {
+        registros->AX = (uint8_t)valor;
+    } else if (strcmp(registroDestino, "BX") == 0) {
+        registros->BX = (uint8_t)valor;
+    } else if (strcmp(registroDestino, "CX") == 0) {
+        registros->CX = (uint8_t)valor;
+    } else if (strcmp(registroDestino, "DX") == 0) {
+        registros->DX = (uint8_t)valor;
+    } else if (strcmp(registroDestino, "EAX") == 0) {
+        registros->EAX = (uint32_t)valor;
+    } else if (strcmp(registroDestino, "EBX") == 0) {
+        registros->EBX = (uint32_t)valor;
+    } else if (strcmp(registroDestino, "ECX") == 0) {
+        registros->ECX = (uint32_t)valor;
+    } else if (strcmp(registroDestino, "EDX") == 0) {
+        registros->EDX = (uint32_t)valor;
+    } else if (strcmp(registroDestino, "SI") == 0) {
+        registros->SI = (uint32_t)valor;
+    } else if (strcmp(registroDestino, "DI") == 0) {
+        registros->DI = (uint32_t)valor;
+    } else {
+        log_error(logger, "Hubo un error al ejecutar la instruccion %s", instruccion);
+        exit(EXIT_FAILURE);
     }
     return EXIT_SUCCESS;
 }
--- cpu/src/main.h
+++ cpu/src/main.h
@@ -19,8 +19,36 @@ typedef enum
     INTERRUPT
 } e_conexiones_kernel;
 
+typedef enum {
+    SET = 0,
+    MOV_IN,
+    MOV_OUT,
+    SUM,
+    SUB,
+    JNZ,
+    RESIZE,
+    COPY_STRING,
+    WAIT,
+    SIGNAL,
+    IO_GEN_SLEEP,
+    IO_STDIN_READ,
+    IO_STDOUT_WRITE,
+    IO_FS_CREATE,
+    IO_FS_DELETE,
+    IO_FS_TRUNCATE,
+    IO_FS_WRITE,
+    IO_FS_READ,
+    INSTRUCTION_EXIT
+} e_instruccion;
+
+
 int main(int argc, char *argv[]);
 void *servidor_dispatch(void *arg);
 void *servidor_interrupt(void *arg);
+
+void fetch(char* linea_de_instruccion, uint32_t PC, t_log *logger;);
+void decode(char* linea_de_instruccion,e_instruccion* instruccion ,char** linea_de_instruccion_separada, t_log *logger;);
+void execute(char** linea_de_instruccion_separada, e_instruccion* instruccion, t_registros *registros, t_log *logger;);
+void checkInterrupt(); 
 
 #endif
